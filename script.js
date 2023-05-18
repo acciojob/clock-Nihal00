5324c4504793bd91f82d2453ac53d7d85f66666d
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-18 20:10:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updatetimer(){
+	const now = new Date();
+
+	const dateTimeString = now.toLocalesString();
+
+	const timerContainer = document.getElementById('timer');
+
+	timerContainer.textContent = dateTimeString;
+}
+
+updatetimer();
+
+setInterval(updatetimer, 1000);
